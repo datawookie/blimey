6f97b1538aef56c931069cfbc6112fc966f30ab4
Andrew Collier <andrew@fathomdata.dev>
2022-07-16 07:21:45 +0100

    feat: Add pivoted version of railways data

--- scripts/prepare-railways.R
+++ scripts/prepare-railways.R
@@ -1,5 +1,6 @@
 library(here)
 library(dplyr)
+library(tidyr)
 library(readr)
 library(janitor)
 library(usethis)
@@ -24,4 +25,12 @@ railways <- read_csv(
     lon_end = end_long
   )
 
+# Pivot data.
+#
+railways_pivot <- railways %>%
+  pivot_longer(matches("^(lat|lon)_"), names_to = "key", values_to = "value") %>%
+  extract(key, c("coord", "extremum"), "([[:alnum:]]+)_([[:alnum:]]+)") %>%
+  pivot_wider(names_from = "coord", values_from = "value")
+
 use_data(railways, overwrite = TRUE)
+use_data(railways_pivot, overwrite = TRUE)
